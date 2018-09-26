0dc35e643ff91a55fffff993b9cd44f57a56d9cb
jeffrey <jeffreyneilflynn@gmail.com>
2018-09-26 16:11:05 -0400

    init chapter 2

--- cracking_the_coding_interview/chapter_01---ArraysStrings/index.js
+++ cracking_the_coding_interview/chapter_01---ArraysStrings/index.js
@@ -63,5 +63,18 @@ const URLify = str => {
 }
 
 // const sample = "jeffrey flynn is awesome";
-
 // console.log("result: ", URLify(sample));
+
+
+
+/*
+Given a string, write a function to check if it is a permutation of a palindrome.
+
+A palindrome is a word or phrase that is the same forwards and backwards.
+
+A permutation is a rearrangement of letters.
+
+The palindrome does not need to be limited to just dictionary words.
+*/
+
+const is_permutation_palindrome = str => {}
