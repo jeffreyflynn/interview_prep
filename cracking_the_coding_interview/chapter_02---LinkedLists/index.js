08623f2046acf6cf4cb9ec8e7eab60b95837549d
jeffrey <jeffreyneilflynn@gmail.com>
2018-09-27 10:45:15 -0400

    add to tail

--- cracking_the_coding_interview/chapter_02---LinkedLists/index.js
+++ cracking_the_coding_interview/chapter_02---LinkedLists/index.js
@@ -1,5 +1,10 @@
 /* LINKED LISTS
 
+  Three basic operations:
+    * searching
+    * insertion
+    * deletion
+
 */
 
 class LinkedList {
@@ -7,6 +12,26 @@ class LinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  add_to_tail = data => {
+    if (!this.head) {
+      const node = new Node(data, null, null);
+      this.head = node;
+      this.tail = node;
+    }
+    else if (this.head === this.tail) {
+      const node = new Node(data, this.head, null);
+      this.head.next = node;
+      this.tail = node;
+    }
+    else {
+      const node = new Node(data, this.tail, null);
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
+
+  // Write code to remove duplicates from an unsorted linked list.
 }
 
 class Node {
@@ -17,5 +42,6 @@ class Node {
   }
 }
 
+/*******************************************************************/
 
-// Write code to remove duplicates from an unsorted linked list.
+const list = new LinkedList();
