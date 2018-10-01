c9f9962abec67c0dbf52bc5e37e242ca4473646d
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-01 15:21:15 -0400

    updated to directed graph class

--- cracking_the_coding_interview/chapter04_Trees&Graphs.js
+++ cracking_the_coding_interview/chapter04_Trees&Graphs.js
@@ -1,16 +1,16 @@
 // Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 // directed graph i.e. each node can only point to one other node
 
+// example: A -> B -> C -> A
 
-class Node {
-  constructor(name) {
-    this.name = name;
-    this.children = [];
-  }
-}
-
-class Graph {
+class DirectedGraph {
   constructor() {
-    this.nodes = [];
+    this.graph = {};
   }
+
+  add_node = (name, neighbor) => {
+    this.graph[name] = neighbor;
+  }
+
+  route_exists = (node_a, node_b) => {}
 }
