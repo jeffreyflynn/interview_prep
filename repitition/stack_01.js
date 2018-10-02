9310a06b6cdbc4fba896c5f4cc8c16c018686b1a
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-02 10:57:20 -0400

    efficient push

--- repitition/stack_01.js
+++ repitition/stack_01.js
@@ -38,3 +38,28 @@ class Stack {
     return this.queue1.dequeue();
   }
 }
+
+
+// efficient push
+class Stack {
+  constructor() {
+    this.queue1 = new Queue();
+    this.queue2 = new Queue();
+    this.size = 0;
+  }
+
+  push = data => {
+    this.queue1.enqueue(data);
+  }
+
+  pop = () => {
+    while (this.queue1.length > 1) {
+      this.queue2.enqueue(this.queue1.dequeue());
+    }
+    const result = this.queue1.dequeue(); // queue1 is now empty
+    const temp = this.queue1;
+    this.queue1 = this.queue2;
+    this.queue2 = temp;
+    return result;
+  }
+}
