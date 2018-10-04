09f849526e7b8bb1a80147b4694dc9481a013409
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-04 16:45:41 -0400

    notes

--- repitition/reverse_linkedlist_01.js
+++ repitition/reverse_linkedlist_01.js
@@ -38,27 +38,53 @@ class LinkedList {
 }
 
 
-const reverseLinkedList = ogList => {
-  if (!ogList.head || !ogList.tail) return null; // nothing to do here
+/*
 
-  const stackOfNodes = [];
-  let current = ogList.head;
+  0. if there is no head or no tail, then there is no work to be done
 
+  1. initialize a stack
+      - this will temporarily hold all list elements
+      - stack = linear data structure that follows LIFO = plain old js array
+
+  2. starting at the head of the original list, push each list element to the stack
+      - head = stack[0] 
+      - tail = stack[length-1]
+
+  3. initialize a new linked list (this will act as the reversed list) 
+      - the last element of the stack will be the head of this new list
+      - the first element of the stack will be the tail of this new list
+
+  4. pop all elements from the stack until none remain
+      a. pop element from stack
+      b. push element to our reversed list
+      c. repeat until stack is empty 
+
+*/
+
+
+const reverseLinkedList = original_list => {
+  // Step 0
+  if (!original_list.head || !original_list.tail) return null; 
+
+  // Step 1
+  const stack = [];
+
+  // Step 2
+  let current = original_list.head;
   while (current) {
-    stackOfNodes.push(current);
+    stack.push(current);
     current = current.next;
   }
 
+  // Step 3
   const reversed = new LinkedList();
 
-  current = stackOfNodes.pop();
-  reversed.head = current;
-  current = stackOfNodes.pop();
-
-  while (current) {
-    current.next = null;
-    reversed.insert(current);
-    current = stackOfNodes.pop();
+  // Step 4
+  let node = stack.pop();
+  while (node) {
+    node.next = null;
+    reversed.insert(node);
+    node = stack.pop();
   }
 
   return reversed;
