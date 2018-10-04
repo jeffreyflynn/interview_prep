7657943b10e86b5d4891983cf1805d0aa724a028
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-04 10:59:49 -0400

    doubly boi

--- repitition/linkedlist.js
+++ repitition/linkedlist.js
@@ -1,7 +1,8 @@
 class Node {
-  constructor(value, next=null) {
+  constructor(value, next=null, prev=null) {
     this.value = value;
     this.next = next;
+    this.prev = prev;
   }
 }
 
@@ -53,5 +54,44 @@ class SinglyLinkedList {
     this.head = newhead;
     
     return this.head;
+  }
+}
+
+
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToTail(value) {
+    const newnode = new Node(value, null, this.tail);
+
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = newnode;
+    }
+
+    else {
+      this.tail.next = newnode;
+      newnode.prev = this.tail;
+      this.tail = newnode;
+    }
+  }
+
+  addToHead(value) {
+    const newnode = new Node(value);
+
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = newnode;
+    }
+
+    else {
+      this.head.prev = newnode;
+      newnode.next = this.head;
+      this.head = newnode;
+    }
   }
 }
