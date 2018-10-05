0eec2634667800b81522af54a0c2145d9562a147
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-05 15:11:15 -0400

    iterative and recursive bst classes

--- repitition/bst_00.js
+++ repitition/bst_00.js
@@ -28,24 +28,85 @@ class Node {
 }
 
 
-class BinarySearchTree {
+// RECURSIVE BINARY SEARCH TREE
+class Recursive_BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value, root=this.root) {
+    if (!this.root) {
+      this.root = new Node(value);
+      return;
+    }
+
+    if (value > root.value) {
+      if (!root.right) root.right = new Node(value);
+      else this.insert(value, root.right);
+    } else {
+      if (!root.left) root.left = new Node(value);
+      else this.insert(value, root.left);
+    }
+  }
+
+  search(value, current=this.root) {
+    // if there is no current node,
+    // or if the current node's value equals the input, 
+    // return current node
+    if (!current) {
+      console.log(current);
+      return current;
+    }
+
+    if (current.value === value) {
+      console.log(current);
+      return current;
+    }
+
+    // if the input val is greater than the current node's value,
+    // then search the right child node
+    if (value > current.value) this.search(value, current.right);
+
+    // if the input val is less than the current node's value,
+    // then search the left child node
+    if (value < current.value) this.search(value, current.left);
+  }
+
+  inOrder(current=this.root) {
+    if (current) {
+      this.inOrder(current.left);
+      console.log(current.value);
+      this.inOrder(current.right);
+    }
+  }
+}
+
+
+// ITERATIVE BINARY SEARCH TREE
+class Iterative_BinarySearchTree {
   constructor() {
     this.root = null;
   }
 
   insert(value) {
     const newnode = new Node(value);
+
+    if (!this.root) {
+      this.root = newnode;
+      return;
+    }
+    
     let current = this.root;
 
     while(true) {
-      if (value > current) {
+      if (value > current.value) {
         if (current.right) current = current.right;
         else {
           current.right = newnode;
           break;
         }
       }
-      else if (value < current) {
+      else if (value < current.value) {
         if (current.left) current = current.left;
         else {
           current.left = newnode;
@@ -56,18 +117,37 @@ class BinarySearchTree {
     }
   }
 
-  search(value, current=this.root) {
-    // if there is no current node,
-    // or if the current node's value equals the input, 
-    // return current node
-    if (!current || current.value === value) return current;
+  search(value) {
+    if (!this.root) return null;
 
-    // if the input val is greater than the current node's value,
-    // then search the right child node
-    if (value > current.value) this.search(value, current.right);
+    let current = this.root;
 
-    // if the input val is less than the current node's value,
-    // then search the left child node
-    if (value < current.value) this.search(value, current.left);
+    while (current) {
+      if (current.value === value) return current.value;
+
+      if (value < current.value) current = current.left;
+
+      else current = current.right;
+    }
+
+    return null;
   }
 }
+
+
+// Let's populate out Binary Search Tree.
+
+// const bst = new Recursive_BinarySearchTree();
+const bst = new Iterative_BinarySearchTree();
+
+bst.insert(111);
+bst.insert(4);
+bst.insert(21);
+bst.insert(30);
+bst.insert(7);
+bst.insert(65);
+bst.insert(48);
+
+bst.search(5);
+
+// bst.inOrder();
