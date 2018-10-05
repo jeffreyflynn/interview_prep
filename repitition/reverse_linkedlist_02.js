2e23138e567ae0d44533edc93eb2d7cf68fcf66c
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-05 13:46:24 -0400

    notes for recursive linked list reversal

--- repitition/reverse_linkedlist_02.js
+++ repitition/reverse_linkedlist_02.js
@@ -82,6 +82,77 @@ class LinkedList {
     // reverse the next two elements
     this.reverse_recurse(_next, _curr);
   }
+
+  /* VISUAL STEP-BY-STEP --- REVERSE A LINKED LIST RECURSIVELY
+  
+    original linked list:
+
+      | 0 |  -->  | 1 |  -->  | 2 |  -->  | 3 |  -->  null
+
+          * { value: 0, next: 1 }
+          * { value: 1, next: 2 }
+          * { value: 2, next: 3 }
+          * { value: 3, next: null }
+
+
+    1st loop:
+      * _curr = this.head = { value: 0, next: 1 }
+      * _prev = null
+      * 
+      * _next = { value: 1, next: 2 }
+      * 
+      * _curr.next = this.head.next = _prev = null
+      * _curr = { value: 0, next: null }
+      * 
+      * recurse - call function on itself with params _curr and _prev
+        * _curr = _next = { value: 1, next: 2 }
+        * _prev = _curr = { value: 0, next: null }
+    
+    null  <--  || 0 ||   -~-   | 1 |  -->  | 2 |  -->  | 3 |  -->  null
+    
+    2nd loop:
+      * _curr = { value: 1, next: 2 }
+      * _prev = { value: 0, next: null }
+      * 
+      * _next = { value: 2, next: 3 }
+      * 
+      * _curr.next = _prev = { value: 0, next: null }
+      * _curr = { value: 1, next: 0 }
+      * 
+      * recurse - call function on itself with params _curr and _prev
+        * _curr = _next = { value: 2, next: 3 }
+        * _prev = _curr = { value: 1, next: 0 }
+      
+    null  <--  | 0 |  <--  || 1 ||   -~-   | 2 |  -->  | 3 |  -->  null
+
+    3rd loop: 
+      * _curr = { value: 2, next: 3 }
+      * _prev = { value: 1, next: 0 }
+      * 
+      * _next = _curr.next = { value: 3, next: null }
+      * 
+      * _curr.next = _prev = { value: 1, next: 0 }
+      * _curr = { value: 2, next: 1 }
+      * 
+      * recurse with @param _curr = _next = { value: 3, next: null }
+      * recurse with @param _prev = _curr = { value: 2, next: 1 }
+    
+    null  <--  | 0 |  <--  | 1 |  <--  || 2 ||   -~-   | 3 |  -->  null
+
+    4th loop: 
+      * _curr = { value: 3, next: null }
+      * _prev = { value: 2, next: 1 }
+      * 
+      * There is no next element. Time to set a new head pointer.
+      * 
+      * this.head = _curr = { value: 3, next: null }
+      * this.head.next = _prev = { value: 2, next: 1 }
+      * 
+      * this.head = { value: 3, next: 2 }
+    
+    null  <--  | 0 |  <--  | 1 |  <--  | 2 |  <--  || 3 ||
+  
+  */
 }
 
 
