1d4378497e6724caa277b4850316674f539ab8d8
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-05 13:16:08 -0400

    reverse linked list - recursive

--- repitition/reverse_linkedlist_02.js
+++ repitition/reverse_linkedlist_02.js
@@ -43,7 +43,9 @@ class LinkedList {
     console.log(result);
   }
 
-  reverse() {
+  // iterative approach
+  // time complexity: O(n)  ~  space complexity: O(1)
+  reverse_iterative() {
     let _prev = null, 
         _curr = this.head, 
         _next = null;
@@ -57,6 +59,29 @@ class LinkedList {
 
     this.head = _prev;
   }
+
+  // recursive approach
+  // @param _curr  |  @type Node  |  @default this.head
+  // @param _prev  |  @type Node  |  @default null
+  // time complexity: O(n)  ~  space complexity: O(1)
+  reverse_recurse(_curr=this.head, _prev=null) {
+    // if this is the last element of the linked list,
+    // set the head pointer to this last element of the original linked list,
+    // and set the new head's next node to the element that originally came before it
+    if (!_curr.next) {
+      this.head = _curr;
+      this.head.next = _prev;
+      return;
+    }
+
+    // else
+    // set a temp variable to the next element
+    let _next = _curr.next;
+    // set the current nodes next value to the node that originally came before it
+    _curr.next = _prev;
+    // reverse the next two elements
+    this.reverse_recurse(_next, _curr);
+  }
 }
 
 
@@ -65,15 +90,15 @@ class LinkedList {
 
 const l = new LinkedList();
 
+l.insert(1);
+l.insert(2);
 l.insert(3);
-l.insert(13);
-l.insert(7);
 
 console.log("\n\n")
 l.print();
 console.log("\n\n")
 
-l.reverse();
+l.reverse_recurse();
 
 console.log("\n\n")
 l.print();
