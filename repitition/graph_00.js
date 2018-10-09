2083c7707ca2a12225731bcfb48887041b825858
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-09 17:50:56 -0400

    init reverse ll reps

---
+++ repitition/graph_00.js
@@ -0,0 +1,28 @@
+/* GRAPHS *//*
+
+A graph is a collection of nodes with edges between them.
+
+    | A |  ---------  | B |
+
+        * A and B are verticies i.e. nodes.
+        * The line connecting A and B is called an edge i.e. connection.
+
+  
+*/
+
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.connections = null;
+  }
+}
+
+
+// class Graph {
+//   constructor() {
+//     this.root = null;
+//   }
+
+//   insert(value, )
+// }
