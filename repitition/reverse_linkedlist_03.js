4672b6740ab094f7daa96e3c70ca301083f80353
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-09 17:53:22 -0400

    insert node

--- repitition/reverse_linkedlist_03.js
+++ repitition/reverse_linkedlist_03.js
@@ -5,3 +5,30 @@ class Node {
     this.next = null;
   }
 }
+
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insert(val) {
+    const newnode = new Node(val);
+
+    if (!this.head) {
+      this.head = newnode;
+      this.tail = newnode;
+    } else {
+      newnode.prev = this.tail;
+      this.tail.next = newnode;
+      this.tail = newnode;
+    }
+  }
+
+  reverse() {
+    if (!this.head) return null;
+
+    
+  }
+}
