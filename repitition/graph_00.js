be474e9e4256ce1b4cbafdef98a02279e3dfa710
jeffrey <jeffreyneilflynn@gmail.com>
2018-10-11 10:44:29 -0400

    delete vertex

--- repitition/graph_00.js
+++ repitition/graph_00.js
@@ -24,7 +24,7 @@ class Graph {
     this.verticies = {};
   }
 
-  addVertex(val) {
+  add_vertex(val) {
     if (!this.verticies[val]) {
       this.verticies[val] = new Node(val);
     } else {
@@ -32,27 +32,46 @@ class Graph {
     }
   }
 
-  addEdge(x_value, y_value) {
+  // edge is similar to an arrow - leads from one node to another
+  add_directed_edge(from_value, to_value) {
     let newnode;
 
-    if (!this.verticies[x_value]) {
-      newnode = new Node(x_value);
-      this.verticies[x_value] = newnode;
+    if (!this.verticies[from_value]) {
+      newnode = new Node(from_value);
+      this.verticies[from_value] = newnode;
     }
 
-    if (!this.verticies[y_value]) {
-      newnode = new Node(y_value);
-      this.verticies[y_value] = newnode;
+    if (!this.verticies[to_value]) {
+      newnode = new Node(to_value);
+      this.verticies[to_value] = newnode;
     }
 
-    this.verticies[x_value].edges.push(y_value);
-    this.verticies[y_value].edges.push(x_value);
+    this.verticies[from_value].edges.push(to_value);
+  }
+
+  remove_vertex(val) {
+    if (!this.verticies[val]) return null;
+    delete this.verticies[val];
+    return;
   }
 
   printGraph() {
     const res = JSON.stringify(this.verticies);
-    console.log(res);
+    console.log(`\n\n ${res} \n\n`);
   }
 }
 
 
+// **************************************************
+
+const g = new Graph();
+
+g.add_vertex(1);
+g.add_vertex(2);
+g.add_vertex(3);
+
+g.add_directed_edge(1, 2);
+g.add_directed_edge(2, 3);
+g.add_directed_edge(3, 1);
+
+g.printGraph();
